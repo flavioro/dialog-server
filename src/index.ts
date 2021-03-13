a45cd2c90cb3c1cea0c872ab45c8213dce8f8f21
flavioro <flavioro@gmail.com>
2021-03-13 18:37:13 -0300

    Add userService and read users.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-const express = require('express');
-const cors = require('cors')
+import express from 'express'
+// require('dotenv').config()
+import cors from 'cors'
+
+import service from './services/UserService'
+
+console.log(service.find())
 
 const app = express()
-
 app.use(cors())
+const port = process.env.PORT_API || 4000
 
-const port = process.env.PORT || 4000
+// console.log(service.find())
+
 app.listen(port, () => {
   console.log(`Your server is running on port ${port}`)
 })
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,16 +1,27 @@
-import UserDb from '../db/users.json'
+// import* as UserDb from '../db/users.json'
+import fs from 'fs'
+import { promisify } from 'util'
 import User from '../dtos/ICreateUserDTO'
 
-// type FunctionFilter = (value: User, index: number) => boolean
+type FunctionFilter = (value: User, index: number) => boolean
 
-export class UserService {
-  static find = (filter?): Promise<User[]> => {
-    const users = UserDb.filter(user => filter(user, user.index))
+export default class UserService {
+  static find = async () => {
+    try {
+      const readFileAsync = promisify(fs.readFile);
+      let users = await readFileAsync('src/db/users.json', { encoding: 'utf-8' })
+      users = JSON.parse(users)
+      // console.log(users[0])
+      // const filtered = users.filter(user => filter(user, user.index))
+  
+      return users
+    } catch (err) {
+      console.log(err)
+    }
 
-    return filter ? users
   }
 
-  static findById = async (id: string) => {
-    return UserDb.find(user => user._id === id)
-  }
+  // static findById = async (id: string) => {
+  //   return fs.readFileSync('users.json').find(user => user._id === id)
+  // }
 }
